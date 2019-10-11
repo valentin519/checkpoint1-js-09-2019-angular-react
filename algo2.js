d2ad7b533489189dc505e038426d71199c8e5c23
Ornella GRISTI <ornellazaidi@gmail.com>
2019-10-11 11:23:22 +0200

    ex algo2 resolved

--- algo2.js
+++ algo2.js
@@ -16,19 +16,25 @@
 * Tableau d'entrée : [56, 7, 63, 9, 7, 12, 85]
 * Résultat : [7, 85]
 */
+const array = [56, 7, 63, 9, 7, 12, 85]
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
+var minMax = (array) => {
+
+    let min = array[0];
+    let max = array[0];
+    let newArray = [];
+
+    for (let i = 0; i< array.length; i++) {
+     
+      if (array[i] < min) {
+        min = array[i];
       }
-      if (array[i] = max) {
+      else if (array[i]>max){
         max = array[i];
       }
-      return array;
+     
     }
-    array.push(min, max);
-  }
+    newArray.push(min, max);
+   return newArray 
+  };
+  console.log(minMax(array))
