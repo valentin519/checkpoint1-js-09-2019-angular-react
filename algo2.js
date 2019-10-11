ca81d45d973a617eedbfa1db057e86bd709aa9a1
Mohammed-Sebaa <mmohamedsebaa@gmail.com>
2019-10-11 13:34:42 +0200

    Algo terminé, tout a été réctifié et ajout d'array test pour confirmer la validation

--- algo2.js
+++ algo2.js
@@ -1,34 +1,47 @@
 /* 
-* Corriger la fonction minMax()
-* La fonction récupère un tableau de nombres positifs, négatifs ou nuls
-* Et retourne un tableau à 2 entrées contenant la valeur minimale
-* et la valeur maximale du tableau récupéré en entrée
-* 
-* Exemple 1 :
-* Tableau d'entrée : [4, 6, 35, -65, -9, 0, 67]
-* Résultat : [-65, 67]
-* 
-* Exemple 2 :
-* Tableau d'entrée : [-30, 5, 43, 108, -5, -7, 89]
-* Résultat : [-30, 108]
-* 
-* Exemple 3 :
-* Tableau d'entrée : [56, 7, 63, 9, 7, 12, 85]
-* Résultat : [7, 85]
-*/
+ * Corriger la fonction minMax()
+ * La fonction récupère un tableau de nombres positifs, négatifs ou nuls
+ * Et retourne un tableau à 2 entrées contenant la valeur minimale
+ * et la valeur maximale du tableau récupéré en entrée
+ * 
+ * Exemple 1 :
+ * Tableau d'entrée : [4, 6, 35, -65, -9, 0, 67]
+ * Résultat : [-65, 67]
+ * 
+ * Exemple 2 :
+ * Tableau d'entrée : [-30, 5, 43, 108, -5, -7, 89]
+ * Résultat : [-30, 108]
+ * 
+ * Exemple 3 :
+ * Tableau d'entrée : [56, 7, 63, 9, 7, 12, 85]
+ * Résultat : [7, 85]
+ */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
-        max = array[i];
-      }
-      return array;
+var minMax = (array) => {
+    let min = array[0];
+    let max = array[0];
+    console.log(array)
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+        if (array[i] > max) {
+            max = array[i];
+        }
     }
+    array = [];
     array.push(min, max);
-  }
+    console.log(array);
+    return array;
+}
+
+const array1 = [4, 6, 35, -65, -9, 0, 67];
+const array2 = [-30, 5, 43, 108, -5, -7, 89]
+const array3 = [56, 7, 63, 9, 7, 12, 85];
+const array4 = [15, 274, 81, 17, -16, 0]
+
+minMax(array1)
+minMax(array2)
+minMax(array3)
+minMax(array4)
