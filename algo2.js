4324ecda913d572fd58987fd6e7ae123f0d1504f
Tass2T <brunog90@gmail.com>
2019-10-11 10:23:19 +0200

    exo 2 done

--- algo2.js
+++ algo2.js
@@ -17,18 +17,13 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
-        max = array[i];
-      }
-      return array;
-    }
-    array.push(min, max);
-  }
+const minMax = (array) => {
+    let max = 0
+    let min = 0
+    min = Math.min(...array)
+    max = Math.max(...array)
+    const tab = [min, max]
+    console.log(tab)
+}
+
+minMax([4, 6, 35, -65, -9, 0, 67])
