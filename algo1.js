2f7513b073da9cb59b9707fc0db78aaf5b8876bf
Jordan Caumartin <pro.jordanc@gmail.com>
2019-10-11 12:45:13 +0200

    resolution excercices

--- algo1.js
+++ algo1.js
@@ -17,3 +17,40 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+
+
+
+
+
+/* JE VAIS DEVOIR CREER UN TABLEAU A DEUX DIMENSION AVEC 26 LIGNES ET 100 COLONNES*/
+
+let lignes = new Array();
+
+for(let i=1; i<27; i++)
+   lignes[i] = new Array();
+
+for(let i=1; i<27; i++)
+
+for(let j=1; j<101; j++)
+      ligne[i][j] = 0;
+
+      for(let i=1; i<27; i++)
+      for(let j=1; j<101; j++)
+          console.log(i + "-" + j);
+
+/* Pour la function je pense qu'il faudrait utiliser map mais j'ai aucune idée de comment faire*/
+/* je vais devoir céer une function qui va avoir une boucle dans un boucle.
+
+  La premiere pour boucle sur elle meme 26 fois pour avoir les 26 lignes. et une autre qui va boucler sur les 26 mais 100 fois. Pour avoir 1-1 1-2 etc*/
+
+
+function NbChaises = () => {
+  const nbLigne = 26;
+  const nbChaisesParLigne = 100
+
+  for (let i=0, i < nbLigne, i++){
+    console.log()
+  }
+
+}
--- algo2.js
+++ algo2.js
@@ -17,18 +17,78 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
-        max = array[i];
-      }
-      return array;
+// var minMax = (array) => {
+//     const min = 0;
+//     const max = 1;
+//     newArray=[];
+
+//     for (let i = 0; i = tableau.length; i++) {
+//     /*Je vais boucler dans mon tableau autant de fois que ça longeur pour savoir si : */  
+
+//     /* Si l'indice, qui par de 0 jusqu'a i qui est egale à la longeur du tableau, est plus petit que min alors il devient min */
+//       if (tableau[i] <= min) {
+//         min = tableau[i];
+//       };
+
+//     /* Si l'indice, qui par de 0 jusqu'a i qui est egale à la longeur du tableau, est plus grand que max alors il devient max */
+
+//       if (tableau[i] >= max) {
+//         max = tableau[i];
+//       };
+
+//     /* Je met tout ça dans un novueau tableau vide*/
+//       newArray.push(min, max);
+
+//       return newArray;
+//     }
+// }
+
+// minMax()
+
+// let array = [-30, 5, 43, 108, -5, -7, 89]
+
+// var minMax = () => {
+//   let min = 0;
+//   let max = array[0];
+
+//   for (let i = 0; i = array.length-1; i++) {
+    
+//     if (array[i] < min) {
+//       min = array[i];
+//     }
+//     if (array[i] >= max) {
+//       max = array[i];
+//     }
+//     array.push(min, max)
+//   }
+//   console.log (array);
+// }
+
+// minMax()
+
+var minMax = (array) => {
+  let min = array[0];
+  let max = array[0];
+  let nouveauTableau = []
+
+  for (let i = 0; i < array.length; i++) {
+
+    if (array[i] < min) {
+      min = array[i];
+    };
+    if (array[i] >= max) {
+      max = array[i];
     }
-    array.push(min, max);
   }
+  nouveauTableau.push(min, max);
+  return nouveauTableau;
+};
+
+minMax()
+
+
+/* Ce que j'ai pas compris :
+
+- le (array) dans la fonction
+
+*/
