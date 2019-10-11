ec5e94dbeea77cc576e8f2be9f3a302e25b8eaa5
Ludivine Coujard <ludcoujard@gmail.com>
2019-10-11 09:42:23 +0200

    petite correction sur algo2.js pour éviter boucle inutile

--- algo2.js
+++ algo2.js
@@ -22,7 +22,7 @@ const minMax = (array) => {
   let min = array[0];
   let max = array[0];
   
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 1; i < array.length; i++) {
     if (array[i] < min) {
       min = array[i];
     }
