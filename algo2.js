33dce5f41e53b5aba9dc1720c69c42b6911f2255
Hélène Baudrand <baudrand.helene@gmail.com>
2019-10-11 12:54:24 +0200

    algo2

--- algo2.js
+++ algo2.js
@@ -17,18 +17,20 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
+var minMax = () => {
+    const array = [];
     const min = 0;
     const max = array[0];
   
     for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
+      if (array[i] < min) {
+        min = array[i];
       }
-      if (array[i] = max) {
+      if (array[i] > max) {
         max = array[i];
       }
-      return array;
+      array.push(min, max);
     }
-    array.push(min, max);
+    return array;
   }
+  console.log(minMax())
