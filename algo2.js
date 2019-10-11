7bc9622b19f5cb7dc912f44717220a0f35f1541f
Gérald Barbosa <barbosa.gerald@gmail.com>
2019-10-11 10:04:39 +0200

    algo2.js fait

--- algo2.js
+++ algo2.js
@@ -17,18 +17,29 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
-        max = array[i];
-      }
-      return array;
+const minMax = (array) => {
+
+  let min = 1000;
+  let max = 0;
+  const out = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
     }
-    array.push(min, max);
+    else if (array[i] > max) {
+      max = array[i];
+    }
   }
+  out.push(min, max);
+  return out;
+
+}
+
+// Pour vérifier
+const tabs1 = [4, 6, 35, -65, -9, 0, 67];
+const tabs2 = [-30, 5, 43, 108, -5, -7, 89];
+const tabs3 = [56, 7, 63, 9, 7, 12, 85];
+console.log(minMax(tabs1));
+console.log(minMax(tabs2));
+console.log(minMax(tabs3));
