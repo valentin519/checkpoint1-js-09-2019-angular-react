f0d4947bfc7ce9b736bed2bdc1efc90c7157e8ac
Morgan-Leblanc <morgan.leblanc91@gmail.com>
2019-10-11 10:20:55 +0200

    rename branch + algo1

--- algo1.js
+++ algo1.js
@@ -17,3 +17,13 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+const colonnes = () => {
+  const tab = [];
+  for (i = 1; i < 26; i++) {
+    for (j = 1; j <= 100; j++){
+      tab.push(`${i} - ${j}`)
+    }
+  }  return tab;
+}
+console.log(colonnes())
