8ac44057633ca3176f4a27b6e5125562628fb727
Tass2T <brunog90@gmail.com>
2019-10-11 09:45:37 +0200

    exo 1 done

--- algo1.js
+++ algo1.js
@@ -17,3 +17,15 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+
+const tableau = () => {
+  const colonnes = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+  return colonnes.map (item => {
+    for (i=1; i<=100;i++){
+    console.log (`${item} - ${i}`)
+    }
+  })
+}
+
+tableau()
