960960e7e73426996dfcd703ab6ccd08d871b7e4
Jérémy Lalait <jlalait33700@gmail.com>
2019-10-11 09:34:23 +0200

    resolution de algo2.js

--- algo2.js
+++ algo2.js
@@ -17,18 +17,19 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
-        max = array[i];
-      }
-      return array;
+var minMax = function (array){
+  let min = Infinity;
+  let max = -Infinity;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
     }
-    array.push(min, max);
+    if (array[i] > max) {
+      max = array[i];
+    }
   }
+  return [min,max];
+}
+
+  
