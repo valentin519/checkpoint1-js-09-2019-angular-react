7f6100b27ad12680396bfb3568b7708c95d083ef
François Boisselier <francois.boisselier@gmail.com>
2019-10-11 12:09:29 +0200

    css

--- algo1.js
+++ algo1.js
@@ -17,3 +17,13 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+function tableau(){
+  let array=[]
+    for (let i=0; i<27; i++){
+      for (let j=0; j<101; j++){
+        array.push(i,j)
+      }
+    }
+  return array
+  }
