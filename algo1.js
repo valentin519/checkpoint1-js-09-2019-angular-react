98b7f3156623f95d9fd3083e7b12cb60c1869357
Baptiste Nicolas <baptiste_44521@hotmail.fr>
2019-10-11 10:39:49 +0200

    ALgo 1 modifié sans etre sur du résulat

--- algo1.js
+++ algo1.js
@@ -17,3 +17,12 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+let Tab =[]
+const NbPlaces = () =>{
+for(let i = 0; i <= 26; i++){
+  for(let n = 0; n <= 100; n++){}}
+  return Tab.push(n,i)
+}
+
+NbPlaces();
