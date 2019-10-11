cd96b80c10c8a94978e8b30b1f4d6a0416ba1f71
LaurentMahieu <mahieu.l@hotmail.fr>
2019-10-11 12:31:42 +0200

    prob résolu avec map sauf  pour ex3

--- algo2.js
+++ algo2.js
@@ -17,18 +17,29 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
+const minMax = (array) => {
+  let max = array[0];
+  let min = array[0];
   
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
+  
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] <= min) {
+        min = array[i];
       }
-      if (array[i] = max) {
+      else if (array[i] >= max) {
         max = array[i];
       }
-      return array;
+      
     }
-    array.push(min, max);
+      
+    return array.filter((enter) => {
+      return enter === max || enter === min;
+    })
+    
   }
+  tab = [56, 7, 63, 9, 7, 12, 85]
+console.log(minMax(tab));
+
+
+
+  
