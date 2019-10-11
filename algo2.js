eb2694150a9fc1afab55aefbc8d338d2d71ef4b0
Sunny PRITCHARD <pritchardsunny19@gmail.comgit config --listgit config --global user.email pritchardsunny19@gmail.com>
2019-10-11 15:19:02 +0200

    add algo2 exercice

--- algo2.js
+++ algo2.js
@@ -17,18 +17,27 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
+
+const minMax = array => {
+  const arrayMaxMin = [];
+  let min = [0];
+  let max = array[0];
   
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
-        max = array[i];
-      }
-      return array;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
     }
-    array.push(min, max);
+    else if (array[i] > max) {
+      max = array[i];
+    }
+  } 
+    arrayMaxMin.push(min, max);
+    return arrayMaxMin;
   }
+
+  console.log(minMax([4, 6, 35, -65, -9, 0, 67]));
+  console.log(minMax([-30, 5, 43, 108, -5, -7, 89]));
+  console.log(minMax([56, 7, 63, 9, 7, 12, 85]));
+  
+
+  
