5c549ac5d5305df6fe36cf0becb20d9c314c4520
Pauline Drouhin <pauline.drouhin@gmail.com>
2019-10-11 10:04:09 +0200

    first algo exercice resolved

--- algo1.js
+++ algo1.js
@@ -17,3 +17,20 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+
+const theatre = () => {
+  const tab = []
+  for (let i = 1; i <= 26; i++) {
+    for (let j = 1; j <= 100; j++) {
+      tab.push(`${i}-${j}`)
+    }
+  }
+  return console.log(tab)
+}
+
+theatre()
+
+
+
+
