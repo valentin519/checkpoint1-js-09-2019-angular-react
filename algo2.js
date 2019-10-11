f53af34e809421cb6b8a95bde2c2c77d12f19f1f
Bastien Saulnier <bastien.saul@gmail.com>
2019-10-11 12:39:48 +0200

    Algo2 resolution

--- algo2.js
+++ algo2.js
@@ -17,18 +17,41 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
-        max = array[i];
-      }
-      return array;
+/*
+
+J'ai corrigé les erreurs notables de mon point de vue dans l'ecriture de la fonction dans ce commentaire, ... 
+Mais n'ayant pas pu arriver a une solution, j'ai utilisé une autre methode, ce n'est pas demandé dans l'exercice 
+mais ca a le merite de fonctionner... 
+
+
+const minMax = (array )=> {
+  const min = 0;
+  const max = array[0];
+
+  for (let i = 0; i = array.length; i++) {
+    if (array[i] < min) {
+      min = array;
     }
-    array.push(min, max);
+    if (array[i] = max) {
+      max = array[i];
+    }
+    return array;
   }
+  array.push(min, max);
+} */
+
+
+const array = [56, 7, 63, 9, 7, 12, 85]
+
+const minMax = (array) => {
+  const min = Math.min(...array);
+  const max = Math.max(...array);
+  const result = [min, max];
+  return result;
+}
+minMax(array);
+
+
+
+
+
