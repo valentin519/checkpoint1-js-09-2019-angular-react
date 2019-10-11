ee8d90ccea45052e0b11206fe2308e472ee67478
alejolale <sergio.prieto01@outlook.com>
2019-10-11 10:16:55 +0200

    Add funtionality to algo2.js minMax Function

--- algo2.js
+++ algo2.js
@@ -17,18 +17,37 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
+
+
+var minMax = (array) => {
+    let min = 0;
+    let max = array[0];
+    const newArray=[];
+    //solution with reduce method instead of for()mthd
+    /*
+    max = array.reduce((min, p) => p > min ? p : min, array[0]);
+    min = array.reduce((min, p) => p < min ? p : min, array[0]);
+    */
+    
+    for (let i = 0; i < array.length; i++) {
+      //console.log(array[i]);
+      if (array[i] < min) {
+        min = array[i];
       }
-      if (array[i] = max) {
+      if (array[i] > max) {
         max = array[i];
       }
-      return array;
+      else{
+        min = array.reduce((min, p) => p < min ? p : min, array[0]);
+      }
     }
-    array.push(min, max);
+    
+    newArray.push(min);
+    newArray.push(max);
+
+    return newArray;
   }
+
+   
+  const myArray=[56, 7, 63, 9, 7, 12, 85];
+  console.log(minMax(myArray));
