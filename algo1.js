16ae503d537a1a7a2e1a924c057ff4eac0b00226
Nombalier <a.nombalier@gmail.com>
2019-10-11 12:01:44 +0200

    html css exercice

--- algo1.js
+++ algo1.js
@@ -1,8 +1,8 @@
 /*
 Un employé de théatre voudrait afficher la liste de tous les sièges présents dans sa salle principale.
 Dans la salle les sièges sont répartis de la manière suivantes : 
-  - Il y a 26 colonnes de sièges, numérotés de "1" à "26"
-  - Chaque colonne contient 100 sièges, numérotés de "1" à "100"
+  - Il y a 26 colonnes de sièges, numérotés de "1" à "26" COL
+  - Chaque colonne contient 100 sièges, numérotés de "1" à "100" ROW
 
 Au final la liste devra ressembler à : 
 1-1 
@@ -17,3 +17,12 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+var col = 0;
+
+for (var i = 1; i < 26; i++) {
+  col = + i;
+  console.log(col)
+  
+}
+
