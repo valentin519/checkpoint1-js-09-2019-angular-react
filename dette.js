09f16fb41d6f170c8183cd6c44d98991d77b69db
Cindie Jouvin <cindie.jouvin@gmail.com>
2019-10-11 12:57:54 +0200

    version finale mais pas terminée

---
+++ dette.js
@@ -0,0 +1,14 @@
+let dette =[
+    {name='Samuel yants',
+    amount='100€'},
+    {name='Simon Pogg',
+    amount='52563€'},
+    {name='Sophia Maria',
+    amount='562€'},
+    {name='Steve Grant',
+    amount='7865€'},
+    {name='Sylvester Stallone',
+    amount='77897€'},
+    {name='Szen Frent',
+    amount='562€'},
+]
