1a0bb919b56c8e074cdb0f1b22f6bea1b73565fa
KleinosFR <kleinos@hotmail.com>
2019-10-11 09:42:08 +0200

    solved algo2.js

--- algo2.js
+++ algo2.js
@@ -17,18 +17,22 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
+var minMax =  (array) => {
+    let min = array[0];
+    let max = array[0];
+    const minMaxArray =[]
   
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
       }
-      if (array[i] = max) {
+      if (array[i] > max) {
         max = array[i];
       }
-      return array;
+      ;
     }
-    array.push(min, max);
+    minMaxArray.push(min, max);
+    return minMaxArray
   }
+
+console.log(minMax([56, 7, 63, 9, 7, 12, 85]))
