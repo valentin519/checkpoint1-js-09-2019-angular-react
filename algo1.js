81086fcfc307e36be97ad562d5784cde0cda6ce9
Ornella GRISTI <ornellazaidi@gmail.com>
2019-10-11 10:21:50 +0200

    ex algo1 resolved

--- algo1.js
+++ algo1.js
@@ -17,3 +17,11 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+const columnNumber = () => {
+    for(let i = 0; i<=26; i++){
+         for(let j =0; j<=100; j++){
+          console.log(`${i}-${j}`)
+        }       
+    }    
+}
+console.log(columnNumber())
