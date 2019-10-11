d6bb48e114751ba444089895b82de45353efef24
Adrien Imberdis <AdrienImberdis@gmail.com>
2019-10-11 12:57:42 +0200

    add disposition img body.css / correction algo2 sans résolution

--- algo2.js
+++ algo2.js
@@ -17,18 +17,24 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
+const array = [4, 6, 35, -65, -9, 0, 67];
+const finalArray = [];
+
+const minMax = (array) => {
+    let min = array[0];
+    let max = array[0];
   
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
+    for (let i = 1; i < array.length; i++) {
+
+      if (array[i] < min) {
+        min = array[i];
+      };
+
+      if (array[i] > max) {
         max = array[i];
       }
       return array;
     }
-    array.push(min, max);
+    finalArray.push(array[min, max]);
   }
+console.log(finalArray);
