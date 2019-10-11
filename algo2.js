f8b842858db589d962aae4fbef240733fc60074e
FuenCore <fuentes33160@gmail.com>
2019-10-11 11:48:41 +0200

    2nd exercice finish

--- algo2.js
+++ algo2.js
@@ -1,34 +1,42 @@
 /* 
-* Corriger la fonction minMax()
-* La fonction récupère un tableau de nombres positifs, négatifs ou nuls
-* Et retourne un tableau à 2 entrées contenant la valeur minimale
-* et la valeur maximale du tableau récupéré en entrée
-* 
-* Exemple 1 :
-* Tableau d'entrée : [4, 6, 35, -65, -9, 0, 67]
-* Résultat : [-65, 67]
-* 
-* Exemple 2 :
-* Tableau d'entrée : [-30, 5, 43, 108, -5, -7, 89]
-* Résultat : [-30, 108]
-* 
-* Exemple 3 :
-* Tableau d'entrée : [56, 7, 63, 9, 7, 12, 85]
-* Résultat : [7, 85]
-*/
+ * Corriger la fonction minMax()
+ * La fonction récupère un tableau de nombres positifs, négatifs ou nuls
+ * Et retourne un tableau à 2 entrées contenant la valeur minimale
+ * et la valeur maximale du tableau récupéré en entrée
+ * 
+ * Exemple 1 :
+ * Tableau d'entrée : [4, 6, 35, -65, -9, 0, 67]
+ * Résultat : [-65, 67]
+ * 
+ * Exemple 2 :
+ * Tableau d'entrée : [-30, 5, 43, 108, -5, -7, 89]
+ * Résultat : [-30, 108]
+ * 
+ * Exemple 3 :
+ * Tableau d'entrée : [56, 7, 63, 9, 7, 12, 85]
+ * Résultat : [7, 85]
+ */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
-        max = array[i];
-      }
-      return array;
+const array = [4, 6, 35, -65, -9, 0, 67];
+
+var minMax = (array) => {
+    let min = 666;
+    let max = -666;
+    const newArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+        if (array[i] > max) {
+            max = array[i];
+        }
     }
-    array.push(min, max);
-  }
+    newArray.push(min, max);
+    return newArray;
+}
+
+console.log(minMax([4, 6, 35, -65, -9, 0, 67]));
+console.log(minMax([-30, 5, 43, 108, -5, -7, 89]));
+console.log(minMax([56, 7, 63, 9, 7, 12, 85]));
+console.log(minMax([42, 666, 1337, -1234567890]))
