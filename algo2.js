7a4d2a0a0b821e3568099aa3e7968fd8a42adf0a
Dylan Berthier <dylan.berthier13@gmail.com>
2019-10-11 11:21:19 +0200

    Resolution de algo2.js

--- algo2.js
+++ algo2.js
@@ -16,19 +16,24 @@
 * Tableau d'entrée : [56, 7, 63, 9, 7, 12, 85]
 * Résultat : [7, 85]
 */
+array = [4, 6, 35, -65, -9, 0, 67]
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
+let minMax = (array) => {
+    let min = array[0];
+    let max = array[0];
+    const newArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
       }
-      if (array[i] = max) {
+      if (array[i] > max) {
         max = array[i];
+    
       }
-      return array;
     }
-    array.push(min, max);
+      newArray.push(min, max);
+      return newArray
   }
+
+console.log(minMax(array))
