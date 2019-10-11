a21f46dbb7d29980639e709217e2e1254db0344c
Hugo V <eyleteam@gmail.com>
2019-10-11 09:44:56 +0200

    ajout exo algo1 et 2 terminé

--- algo1.js
+++ algo1.js
@@ -17,3 +17,13 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+const arrayOfSeat = []
+
+for (let i=1; i < 27; i++) {
+  for (let j=1; j < 101; j++) {
+    arrayOfSeat.push(`${i} - ${j}`)
+  }
+}
+
+console.log(arrayOfSeat)
--- algo2.js
+++ algo2.js
@@ -17,18 +17,35 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
+
+var minMax = (array) => {
+  let min = 0;
+  let max = 0;
   
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
-        max = array[i];
-      }
-      return array;
+  for (let i=0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    } else if (array[i] > max) {
+      max = array[i];
     }
-    array.push(min, max);
   }
+  return array = [min, max];
+}
+
+
+console.log(minMax([4, 6, 35, -65, -9, 0, 67]))
+
+/*
+function minMax(array) {
+  let min = 0;
+  let max = 0;  
+
+  for (let i=0; i < array.length; i++) {
+    if (max < array[i]) {
+      max = array[i]
+    } else if (min > array[i]) {
+      min = array[i]
+    }
+  }
+  return array = [min, max]
+}*/
