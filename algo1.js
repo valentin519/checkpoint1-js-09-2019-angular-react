83e861438612d1fe8ba0a5a2520e307e1860a8da
Signupz <ludovic.mazet@gmail.com>
2019-10-11 10:01:27 +0200

    Premier algo terminé

--- algo1.js
+++ algo1.js
@@ -1,19 +1,12 @@
-/*
-Un employé de théatre voudrait afficher la liste de tous les sièges présents dans sa salle principale.
-Dans la salle les sièges sont répartis de la manière suivantes : 
-  - Il y a 26 colonnes de sièges, numérotés de "1" à "26"
-  - Chaque colonne contient 100 sièges, numérotés de "1" à "100"
+const chairsList = () => {
+    numbers = '1,2,3,4,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100';
+        return numbers.split(",").map(number => {
+        const chairs = [];
+        for (let i = 1; i <= 100 ; i++) {
+            chairs.push(`${number} - ${i}`);    
+        } 
+        return chairs;
+    });
+}
 
-Au final la liste devra ressembler à : 
-1-1 
-1-2
-1-3
-.
-.
-.
-26-98
-26-99
-26-100
-
-Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
-*/
+console.log(chairsList());
