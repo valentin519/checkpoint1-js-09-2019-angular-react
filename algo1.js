16ca29dfa26778d9fb1f3b3426e0e733c154ea82
aurelia <aurelia.zigman@gmail.com>
2019-10-11 13:05:35 +0200

    commit de algo 1.js et CSS book

--- algo1.js
+++ algo1.js
@@ -15,5 +15,45 @@ Au final la liste devra ressembler à :
 26-99
 26-100
 
-Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
+Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.*/
+
+/*const colonne {
+  return (colonne.lenght [26])
+  for (let i = 0; i < 26; i++) {
+    console.log(i);
+} /*faire une boucle affichant de 1 a 26*/
+
+/*
+const siege {
+return (siege.lenght [100])
+}
+
+afficher la valeur de 1 à 26 dans la colonne < 26
+faire une boucle qui compte jusqu'a 100 dans chaque index du tableau colonne
 */
+
+
+/*const array = [];
+array.length = 26;
+for (var i = 0; i < array.length; i++) {
+  array[i]++;
+}
+array.forEach(element => console.log(element));*/
+
+const colonne = [];
+colonne.length = 26;
+
+const siege = [];
+colonne.length = 100;
+
+array = []; 
+
+for (let i = 0; i < colonne.length; i++) {
+  for (let j = 0, j < siege.length; j++) {
+    array.push(+colonne[i] + "-" +  siege[j]);
+  }
+}
+console.log(array[1]);
+for (let k = 0; k < array.length; k++) {
+  console.log(array[k]);
+}
