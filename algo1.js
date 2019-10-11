ff68459ff4c921a23fa2e7ee15dda0e80b406388
Pauline Roche <rochepauline6@gmail.com>
2019-10-11 09:32:33 +0200

    resolving algo1.js

--- algo1.js
+++ algo1.js
@@ -17,3 +17,19 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+
+function getNumOfSeats(){
+  const seatsList = [];
+
+  for (let i=1; i <= 26; i++){
+    let column;
+    for (let j=1; j<=100; j++){
+      column = `${i} - ${j}`;
+      seatsList.push(column);
+    }
+  }
+  return seatsList;
+}
+
+console.log(getNumOfSeats());
