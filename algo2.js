9231d95661cba55ba2143ee7f9f25a8d318425a3
cdhbze93 <antoebt@hotmail.com>
2019-10-11 11:57:20 +0200

    Fin algo 2

--- algo2.js
+++ algo2.js
@@ -17,18 +17,20 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
-        max = array[i];
-      }
-      return array;
+var minMax = (array) => {
+  let min = 9999999999999999999999999999999999999999999999999999999999999999 + 42;
+  let max = -999999999999999999999999999999999999999999999999999999999999999 - 42;
+  const newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
     }
-    array.push(min, max);
+    if (array[i] > max) {
+      max = array[i];
+    }
+          
   }
+newArray.push(min, max)
+ console.log(newArray);
+}
