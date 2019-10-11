f1df0ac2cd538c344208afe5da22aaab7f49c9d7
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-10-11 09:25:32 +0200

    Fin de l'exercice algo1.

--- algo1.js
+++ algo1.js
@@ -17,3 +17,13 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+const allSeat = [];
+
+for (let i = 1; i < 27; i++){
+  for (let j = 1; j < 101; j++){
+    allSeat.push(`${i} - ${j}`);
+  };
+};
+
+console.log (allSeat);
