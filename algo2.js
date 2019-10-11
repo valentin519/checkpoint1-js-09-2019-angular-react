dde392c799fed664356838f4dee28ab94576c639
Charles Gourdin <gourdin.charles@gmail.com>
2019-10-11 09:58:05 +0200

    Algo 2 résolu

--- algo2.js
+++ algo2.js
@@ -17,18 +17,39 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
+// var minMax = () => (array) => {
+//     const min = 0;
+//     const max = array[0];
   
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
-        max = array[i];
-      }
-      return array;
+//     for (let i = 1; i = array.length - 1; i+1) {
+//       if (array(i) < min) {
+//         min = array;
+//       }
+//       if (array[i] = max) {
+//         max = array[i];
+//       }
+//       return array;
+//     }
+//     array.push(min, max);
+//   }
+
+const minMax = (array) => {
+  let min = array[0];
+  let max = array[0];
+  const minMaxTab=[]
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
     }
-    array.push(min, max);
+    if (array[i] > max) {
+      max = array[i];
+    }
   }
+  minMaxTab.push(min, max);
+  return minMaxTab
+}
+
+const testArray = [4, 6, 35, -65, -9, 0, 67]
+
+console.log(minMax(testArray))
