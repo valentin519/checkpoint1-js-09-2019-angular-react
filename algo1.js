31af2b4ce322b649454d1cd442b4c1fef471c112
Chloe Gimenes <chloegimenes@gmail.com>
2019-10-11 09:39:42 +0200

    Algo1 modifié

--- algo1.js
+++ algo1.js
@@ -17,3 +17,24 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+
+const sitList = [];
+
+
+
+const makeList = () => {
+  for (let i=1; i<27; i++) {
+    
+    for (let j=1; j<101; j++) {
+      
+      sitList.push(`${i} - ${j}`);
+       
+  }
+}
+
+}
+
+makeList();
+
+console.log(makeList);
