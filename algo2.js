8ffe30e209f4e48a03121601dfa6b3f2a8c236de
Vi <dupre.virginie@gmail.com>
2019-10-11 12:48:23 +0200

    Second algo exercise from checkpoint1

--- algo2.js
+++ algo2.js
@@ -17,18 +17,22 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
+  const minMax = (array) => {
+    let min = array[0];
+    let max = array[0];
+    let newArray = [];
+
+    for (let i = 0; i < array.length; i++) {  
   
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
+      if (array[i] < min) {
+        min = array[i];
       }
-      if (array[i] = max) {
+      if (array[i] > max) {
         max = array[i];
       }
-      return array;
     }
-    array.push(min, max);
+    newArray.push(min, max)
+    return newArray
   }
+console.log(minMax(array))
+  
