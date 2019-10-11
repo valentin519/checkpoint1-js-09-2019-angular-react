cb594313f762fd2ed2f890f6439aca5b87a1077c
Valentine Voyen <valentine.voyen1@gmail.com>
2019-10-11 12:22:11 +0200

    resolved algo1.js

--- algo1.js
+++ algo1.js
@@ -17,3 +17,16 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+const cinema = () => {
+  const cinema= [];
+  for (let i = 1; i <= 26 ; i++) {
+    for (let j = 1; j <= 100; j++) {
+      cinema.push(`${i} - ${j}`)
+    } 
+  }
+  return cinema
+}
+console.log(cinema())
+
+
