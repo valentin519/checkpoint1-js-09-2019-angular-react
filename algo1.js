68fd08f16c877ff496f0775758aabaa41e322684
Amaury Neury <amaury.neury@gmail.com>
2019-10-11 12:45:46 +0200

    Mes solutions exercices checkpoint

--- algo1.js
+++ algo1.js
@@ -1,19 +1,14 @@
-/*
-Un employé de théatre voudrait afficher la liste de tous les sièges présents dans sa salle principale.
-Dans la salle les sièges sont répartis de la manière suivantes : 
-  - Il y a 26 colonnes de sièges, numérotés de "1" à "26"
-  - Chaque colonne contient 100 sièges, numérotés de "1" à "100"
-
-Au final la liste devra ressembler à : 
-1-1 
-1-2
-1-3
-.
-.
-.
-26-98
-26-99
-26-100
-
-Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
-*/
+let listSieges = [];
+let nbrCol = 26;
+let nbrRan = 10;
+function seatCounter(){
+    let listSieges = [];
+    for (i=1; i<=nbrCol; i++){
+        for (j=1; j<=nbrRan;j++){
+            listSieges.push(i+"-"+j)
+        }
+    }
+    return listSieges
+    
+}    
+console.log(seatCounter(nbrCol,nbrRan))
