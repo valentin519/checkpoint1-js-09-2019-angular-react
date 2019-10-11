bbb123ffc3920d1c5966d6f43148c72c333a304b
thetinyspark <contact@the-tiny-spark.com>
2019-10-11 14:48:37 +0200

    message EXPLICITE

--- algo1.js
+++ algo1.js
@@ -1,7 +1,7 @@
 /*
 Un employé de théatre voudrait afficher la liste de tous les sièges présents dans sa salle principale.
-Dans la salle les sièges sont répartis de la manière suivantes : 
-  - Il y a 26 colonnes de sièges, numérotés de "1" à "26"
+Dans la salle les sièges sont répartis de la manière suivante : 
+  - Il y a 26 colonnes de sièges, numérotées de "1" à "26"
   - Chaque colonne contient 100 sièges, numérotés de "1" à "100"
 
 Au final la liste devra ressembler à : 
@@ -17,3 +17,34 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+function theater(){
+
+  // on a besoin d'un tableau pour enregistrer l'ensemble de nos résultats
+  let seats = []; 
+
+  // on crée une boucle permettant de créer l'ensemble des colonnes
+  for( let i = 1; i <= 26; i++){
+    // chaque colonne doit contenir une 100aine de sièges
+    // on utilise donc encore une boucle
+
+    // on a besoin de créer un tableau représentant la colonne en cours 
+    let column = [];
+
+    for( let j = 1; j <= 100; j++ ){
+      // nous avons donc à chaque tour de boucle, la variable i qui représente le numéro de la colonne 
+      // et la variable j qui représente le numéro du siège au sein de cette colonne
+      // on pousse dans la colonne en cours la chaîne de caractère suivante: 
+      
+      // column.push( `${i} - ${j}`); // syntaxe template strings
+      column.push( i + "-" + j); // vieille syntaxe
+    }
+
+    // on pousse la colonne ainsi remplie au sein de notre tableau "seats"
+    seats.push(column)
+  }
+
+  return seats;
+}
+
+console.log(theater());
--- algo2.js
+++ algo2.js
@@ -17,18 +17,36 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
-        max = array[i];
-      }
-      return array;
+function minMax(listNums) {
+
+  // on initialise la valeur "min" à 0
+  let min = listNums[0];
+
+  // on initialise la valeur max à ( première valeur de la liste des numéros fournis)
+  let max = listNums[0];
+
+  // pour chaque numéro dans la liste de numéros
+  for (let i = 0; i < listNums.length; i++) {
+
+    // si le numéro en cours est plus petit que la valeur contenue dans "min"
+    if (listNums[i] < min) {
+      // alors on actualise "min" et on lui donne comme valeur, le numéro en cours
+      min = listNums[i];
     }
-    array.push(min, max);
+
+    // si le numéro en cours est plus grand que la valeur contenue de ans "max"
+    if (listNums[i] > max) {
+      // alors on actualise "max" pour lui assigner comme valeur, le numéro en cours
+      max = listNums[i];
+    }
   }
+  
+  // on retourne un tableau contenant les valeurs min et max
+  return [min,max];
+}
+
+let result1 = minMax([4, 6, 35, -65, -9, 0, 67]);
+let result2 = minMax([-30, 5, 43, 108, -5, -7, 89]);
+let result3 = minMax([56, 7, 63, 9, 7, 12, 85]);
+
+console.log(result1, result2, result3);
