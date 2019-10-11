6f28706c0857e621b8b0fa6ff791a20b3c8509e9
Paul Douane <paul@douane.nom.fr>
2019-10-11 10:01:34 +0200

    Finition exercice 2 algo

--- algo2.js
+++ algo2.js
@@ -17,18 +17,24 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
-        max = array[i];
-      }
-      return array;
+var minMax = (array) => {
+  let newArray = [];
+
+  let min = array[0];
+  let max = array[0];
+
+  for (let i = 0; i <= array.length - 1; i++) {
+    if (array[i] < min) {
+      min = array[i];
     }
-    array.push(min, max);
+    if (array[i] > max) {
+      max = array[i];
+    }
   }
+  newArray.push(min, max)
+  return newArray;
+}
+
+console.log(minMax([4, 6, 35, -65, -9, 0, 67]));
+console.log(minMax([-30, 5, 43, 108, -5, -7, 89]));
+console.log(minMax([56, 7, 63, 9, 7, 12, 85]));
