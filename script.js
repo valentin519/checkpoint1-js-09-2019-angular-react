d065b76bbed89a8fbb41f00c560dd834fcc60130
KobeSan <baptiste.gislot@gmail.com>
2019-10-11 11:51:57 +0200

    Ajout de la deuxième façon d'afficher les résultats de l'exo HTML

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const button = document.getElementById('pay');
-let bribes = document.getElementById('bribes');
+const bribeNames = document.getElementById('bribeNames');
+const bribeValues = document.getElementById('bribeValues');
 const total = document.getElementById('total');
 
 let sum = 0
@@ -8,19 +9,37 @@ let value = 0;
 
 let payments = [];
 
+// Ajout de l'event listener pour le clique sur le bouton Pay !
 button.addEventListener('click', () => {
+  
+  // Paramétrage des variables avec les valeurs récupérées dans le form
   name = document.getElementById('name').value;
   value = document.getElementById('payment').value;
 
+  //Ajout des valeurs récupérées dans le form dans un tableau qu'on affichera par la suite
   payments.push([name, value]);
 
-  let bribesHtml = payments.map((payment) => {
+  //map dans le tableau pour afficher chaque nom
+  let bribeNameHtml = payments.map((payment) => {
     return `
-      <p>${payment[0]} ${payment[1]}€</p>
+      <p>${payment[0]}</p>
     `
   });
 
+  //map dans le tableau pour afficher chaque paiement associé au nom
+  let bribeValuesHtml = payments.map((payment) => {
+    return `
+      <p>${payment[1]} €</p>
+    `
+  });
+
+  // Calcul du total des paiements
   sum += parseInt(value, 10);
-  bribes.innerHTML = bribesHtml;
+
+  //Affichage dans le html des paiments effectués 
+  bribeNames.innerHTML = bribeNameHtml;
+  bribeValues.innerHTML = bribeValuesHtml;
+
+  //Affichage du total des paiements
   total.innerHTML = `<hr color="black" size="1" width="50%" align="right"/><p>Total: ${sum}€</p>`
 });
