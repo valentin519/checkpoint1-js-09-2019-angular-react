58409396e93fa13edfc97fdf2486fd1dd5f1645a
jervalles <jer.vallles@outlook.com>
2019-10-11 11:15:35 +0200

    algo2.js fixed

--- algo2.js
+++ algo2.js
@@ -17,18 +17,20 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
+const minMax = (array) => {
+    let min = array[0];
+    let max = array[0];
   
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
       }
-      if (array[i] = max) {
+      if (array[i] > max) {
         max = array[i];
       }
-      return array;
     }
-    array.push(min, max);
-  }
+    newArray = [min, max];
+    return newArray;
+}
+
+console.log(minMax([56, 7, 63, 9, 7, 12, 85]));
