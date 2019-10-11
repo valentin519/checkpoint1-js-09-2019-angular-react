38ab2119a6691067f4a3e4a1b886845fe965ff45
simplonco <jean@gmail.com>
2019-10-11 14:36:56 +0200

    projet

--- algo1.js
+++ algo1.js
@@ -1,19 +1,11 @@
-/*
-Un employé de théatre voudrait afficher la liste de tous les sièges présents dans sa salle principale.
-Dans la salle les sièges sont répartis de la manière suivantes : 
-  - Il y a 26 colonnes de sièges, numérotés de "1" à "26"
-  - Chaque colonne contient 100 sièges, numérotés de "1" à "100"
+const siege = () => {
+    const siege = [];
+    for (let i = 1; i <= 26 ; i++) {
+    for (let j = 1; j <= 100 ; j++) {
+        siege.push(`${i} - ${j}`);
+    }
+    }
+    return siege;
+}
 
-Au final la liste devra ressembler à : 
-1-1 
-1-2
-1-3
-.
-.
-.
-26-98
-26-99
-26-100
-
-Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
-*/
+console.log(siege())
--- algo2.js
+++ algo2.js
@@ -1,39 +1,21 @@
-/* 
-* Corriger la fonction minMax()
-* La fonction récupère un tableau de nombres positifs, négatifs ou nuls
-* Et retourne un tableau à 2 entrées contenant la valeur minimale
-* et la valeur maximale du tableau récupéré en entrée
-* 
-* Exemple 1 :
-* Tableau d'entrée : [4, 6, 35, -65, -9, 0, 67]
-* Résultat : [-65, 67]
-* 
-* Exemple 2 :
-* Tableau d'entrée : [-30, 5, 43, 108, -5, -7, 89]
-* Résultat : [-30, 108]
-* 
-* Exemple 3 :
-* Tableau d'entrée : [56, 7, 63, 9, 7, 12, 85]
-* Résultat : [7, 85]
-*/
-
 const minMax =  (array) => {
-  let min = array [0];
-  let max = array[0];
- let chocolat = [];
- console.log("hi")
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] < min) {
-      min = array[i];
-    }
-    if (array[i] > max) {
-      max = array[i];
+    let min = array [0];
+    let max = array[0];
+   let chocolat = [];
+   console.log("hi")
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
+      }
+      if (array[i] > max) {
+        max = array[i];
+      }
+      chocolat.push(min, max);
     }
     
+    console.log(chocolat)
+    return chocolat;
   }
-  chocolat.push(min, max);
-  console.log(chocolat)
-  return chocolat;
-}
 
-minMax([1,2,3, -2]);
+  minMax([1,2,3, -2]);
+
