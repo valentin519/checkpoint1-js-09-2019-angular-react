a3dad122522b09f99914630c7199d6df59275a07
Sophie <strochain@gmail.com>
2019-10-11 11:57:59 +0200

    algo1 démarré, html et css travaillés sans succès

--- algo1.js
+++ algo1.js
@@ -17,3 +17,25 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+const column = [];
+const seats = [];
+
+
+for (let i=1; i<27; i++) {
+  column.push(i);
+}
+
+console.log(column)
+
+for (let j=1; j<101; j++) {
+  seats.push(j);
+}
+
+console.log(j);
+
+
+
+
+
+
