0a59d2da417364844c54f2b2dcd61367cd6ac19d
stéphane <gervaisstephane@orange.fr>
2019-10-11 11:34:53 +0200

    algo1 fini

--- algo1.js
+++ algo1.js
@@ -17,3 +17,14 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+
+const board = [];  
+
+for (let i = 0; i < 26; i++){ 
+  board[i] = [];             
+for (let j = 0; j < 100; j++){ 
+  board[i][j]= `${(i + 1)}-${j + 1}`; 
+}
+}
+console.log(board);;
