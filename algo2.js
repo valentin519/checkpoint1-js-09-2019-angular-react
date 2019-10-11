34e466328231a98fb9f2c0370aec7559a599f633
Valentine Voyen <valentine.voyen1@gmail.com>
2019-10-11 13:04:43 +0200

    resolved algo2.js

--- algo2.js
+++ algo2.js
@@ -17,18 +17,23 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
+const minMax = (array) => {
+    let min = 0;
+    let max = 0;
+    const tab = [];
+    for (let i = 0; i < array.length; i++) {
+      if (i === 0){
+        min = array[i];
       }
-      if (array[i] = max) {
+      else if (array[i] < min) {
+        min = array[i];
+      
+       } else if (array[i] > max) { 
         max = array[i];
-      }
-      return array;
+       }
     }
-    array.push(min, max);
+    tab.push(min, max);
+    return tab;
   }
+  
+  console.log(minMax([4, 6, 35, -65, -9, 0, 67]))
