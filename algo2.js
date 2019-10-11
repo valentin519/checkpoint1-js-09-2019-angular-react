fd346a67f0973c13e8cef78f0c7764ea65846842
Mathieu GAUBAL-VATILINGON <b_jayson@hotmail.com>
2019-10-11 10:24:07 +0200

    algo2.js : working progress

--- algo2.js
+++ algo2.js
@@ -17,18 +17,23 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
-        max = array[i];
-      }
-      return array;
+/* working progress */
+var minMax = (array) => {
+  let min = 0;
+  let max = 0;
+  let newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
     }
-    array.push(min, max);
+    if (array[i] > max) {
+      max = array[i];
+    } 
   }
+newArray.push(min, max);  
+return newArray;
+}
+const array = [4, 6, 35, -65, -9, 0, 67];
+const resultat = minMax(array);
+console.log(resultat);
