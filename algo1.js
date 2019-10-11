4457947262782c0519d58cf24607d6eb6fd6a824
Jean Nicolas de Lamballerie <jean.nicolasdelamballerie@gmail.com>
2019-10-11 09:51:52 +0200

    Resolved algorithmic

--- algo1.js
+++ algo1.js
@@ -17,3 +17,13 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+const chairs = () => {
+const chairsArray = []
+  for (let i = 1; i < 27; i++) {
+    for (let j = 1; j < 101; j++) {
+      chairsArray.push(`${i}-${j}`)
+    }
+  }
+return chairsArray
+}
+ 
--- algo2.js
+++ algo2.js
@@ -17,18 +17,19 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
+var minMax = (array) => {
+    let min = array[0];
+    let max = array[0];
+    const returnArray= []
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
       }
-      if (array[i] = max) {
+      if (array[i] > max) {
         max = array[i];
       }
-      return array;
     }
-    array.push(min, max);
+  returnArray.push(min, max);
+  return returnArray;
   }
+console.log(minMax([56, 7, 63, 9, 7, 12, 85]))
