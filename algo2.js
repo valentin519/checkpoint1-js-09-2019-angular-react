b0f15871099c2863e92931e9f3271d5243964134
psycho33200 <pininfarina47@gmail.com>
2019-10-11 09:58:47 +0200

    resolve algo2.js

--- algo2.js
+++ algo2.js
@@ -17,18 +17,20 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
+const minMax = (array) => {
+    const min = 1000;
+    const max = 0;
+    let minMaxTwo = []
   
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
+    for (let i = 0; i = array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
       }
-      if (array[i] = max) {
+      if (array[i] > max) {
         max = array[i];
       }
-      return array;
+      
     }
-    array.push(min, max);
+    minMaxTwo.push(min, max);
+    return minMaxTwo
   }
