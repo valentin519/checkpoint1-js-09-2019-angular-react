28cc7632e80ce41c6bcb8d833709740d9df1512d
Aloxbro <alox51@wanadoo.fr>
2019-10-11 10:40:38 +0200

    algo1 resolved

--- algo1.js
+++ algo1.js
@@ -17,3 +17,16 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+
+const totalSeats = []
+function arraySeat(array){
+  for (let i = 1; i <= 26; i++){
+    for (let j = 1; j <= 100; j++){
+      array.push(i + '-' +j);
+    }
+  }
+  return array
+}
+
+arraySeat(totalSeats)
