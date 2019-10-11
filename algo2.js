2b4218d2ccaa1590a1d7bd0a86054a07dfaebe60
Armand72 <armandmeunier@hotmail.fr>
2019-10-11 09:59:16 +0200

    exercise number two finished

--- algo2.js
+++ algo2.js
@@ -17,18 +17,23 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
+const minMax = (array) => {
+    let min = array[0];
+    let max = array[0];
+    let table = []
+    
   
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
       }
-      if (array[i] = max) {
+      if (array[i] > max) {
         max = array[i];
       }
-      return array;
+      
     }
-    array.push(min, max);
+    table.push(min, max);
+    return table;
   }
+
+console.log(minMax([56, 7, 63, 9, 7, 12, 85]))
