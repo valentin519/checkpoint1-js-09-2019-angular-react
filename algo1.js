558d2d0735aa0ad8b50e656f767c2a4e5817b3c4
FPaurisse <paurissefrederic@gmail.com>
2019-10-11 09:27:25 +0200

    resolve exercise algo1.js

--- algo1.js
+++ algo1.js
@@ -17,3 +17,13 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+function countPlaces(){
+  let listPlaces = []
+  for(let i = 1; i <= 26; i++){
+    for(let j = 1; j <= 100; j++){
+      listPlaces.push(`${i}-${j}`)
+    }
+  }
+  return listPlaces;
+}
