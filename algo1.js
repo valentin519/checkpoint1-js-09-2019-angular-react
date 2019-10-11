5e51bf3c6b0f8aa3ca0bec2d5304a15304878a97
Martin Rivière <martin.riviere@gmail.com>
2019-10-11 09:26:03 +0200

    completed algo1.js with a function that returns an array with seats numbers

--- algo1.js
+++ algo1.js
@@ -17,3 +17,11 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+const theaterSeats = () => {
+  const seats = [];
+  for (let i = 1; i <= 26; i++) {
+    for (let j = 1; j <= 100; j++) seats.push(`${i}-${j}`);
+  }
+  return seats;
+};
