e3155646c0d0385692367cd316103119cd6f2b14
Vi <dupre.virginie@gmail.com>
2019-10-11 09:46:42 +0200

    commit of the first exercise algo1.js for Checkpoint1

--- algo1.js
+++ algo1.js
@@ -17,3 +17,10 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+let myArray = []
+for (let i = 1; i < 27; i++) {
+  for ( let j = 1; j < 101; j++) {
+    myArray.push(i + '-' + j)
+  }
+}
+return(myArray)
