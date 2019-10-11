f54b900393ad683f652f60c2ced9e528f09706ef
stéphane <gervaisstephane@orange.fr>
2019-10-11 12:18:05 +0200

    algo2 on vera si sa passe

--- algo2.js
+++ algo2.js
@@ -17,18 +17,20 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
+const minMax = (array) => {
     const min = 0;
     const max = array[0];
   
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
+    for (let i = 1; i = array.length + 1; i++) {
+      if (array[i] < min) {
+        min = array
       }
-      if (array[i] = max) {
+      if (array[i] > max) {
         max = array[i];
       }
-      return array;
+      array.push(min, max);
     }
-    array.push(min, max);
+    return array;
   }
+
+
