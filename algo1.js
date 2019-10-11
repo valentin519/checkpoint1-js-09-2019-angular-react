0f0b2f9b4fa4cee9c67004f0c59cc57348a18cdc
Corinne Russo <corinne.russo@gmail.com>
2019-10-11 14:40:38 +0200

    commit de l'algo1

--- algo1.js
+++ algo1.js
@@ -17,3 +17,20 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+const chairNumber=()=>{
+  const array=[];
+  let array2 = [];
+    for (let i=0; i<=26; i++){
+      for (let j=0; j<=100; j++){
+        
+        array2.push("-"+j);
+      }
+      array.push(array2);
+      array2=[];
+      
+    }
+    return array;
+}
+
+chairNumber()
