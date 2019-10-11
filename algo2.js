a9985594a7ea2bf3c0897c501ad5c21c9e49cdae
FPaurisse <paurissefrederic@gmail.com>
2019-10-11 09:53:37 +0200

    resolve exercise algo2

--- algo2.js
+++ algo2.js
@@ -17,18 +17,10 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
-        max = array[i];
-      }
-      return array;
-    }
-    array.push(min, max);
+var minMax = (array) => {
+  let newArray = []
+    let min = Math.min(...array);
+    let max = Math.max(...array);
+      newArray.push(min, max);
+      return newArray;
   }
