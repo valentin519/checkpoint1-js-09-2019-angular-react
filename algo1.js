fefdbcccb0d04e646d1d80b8c56540d2edd3d269
Thomas <tponthoreau@gmail.com>
2019-10-11 09:59:45 +0200

    1er algo

--- algo1.js
+++ algo1.js
@@ -17,3 +17,16 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+function nbSieges(){
+  const sieges = [];
+  for (let i = 1; i <= 26; i++) {
+      const row = [];
+      for (let j = 1; j <= 100; j++) {
+          row.push(`${i} - ${j}`);
+      }
+      sieges.push(row); 
+  }
+  return sieges;
+}
+console.log(nbSieges());
