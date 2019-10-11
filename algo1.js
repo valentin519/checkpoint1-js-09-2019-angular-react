7a2d846339a8f0c97d4a05c3bb9e4679b84b3e86
Nelo <kevinpplle@gmail.com>
2019-10-11 10:14:16 +0200

    Exo Algo1.js fini

--- algo1.js
+++ algo1.js
@@ -17,3 +17,45 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+
+
+// Je déclare ma fonction Place assise 
+const rawSeat = () => {
+  
+  // Deuxième Tableau qui contiendra le résultat final 
+  const raw = []
+
+  // Boucle for : Je déclare mon let i = 1 (on commence à 1 ici et non pas 0), tant que i est inférieur ou égal à 26, j'incrémente i de + 1. (jusqu'à 26)
+  for (let i = 1; i <= 26; i++) {
+
+    // Même procédé ici, boucle for : let ea (emptyArray) = 1, tant que ea est inférieur ou égal à 100, j'incrémente i de + 1. (Jusqu'à 100)
+    for (let ea = 1 ; ea <= 100 ; ea++ ) {
+      // Je cherche une méthode qui va me permettre de pousser mes éléments dans le tableau vide (raw) (raw.push ?)
+      raw.push(`${i} + ${ea}`)
+    }
+ }
+
+ //Je return pour récupérer le résultat de raw.
+ return raw
+}
+
+console.log(rawSeat());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
