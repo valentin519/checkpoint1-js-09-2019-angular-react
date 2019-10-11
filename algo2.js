414a7a5784c183620ce473f8b6833f74b280f641
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-10-11 10:08:57 +0200

    Fin de l'exercice algo2, fonction totalement réécrite.

--- algo2.js
+++ algo2.js
@@ -17,6 +17,7 @@
 * Résultat : [7, 85]
 */
 
+/*
 var minMax = () => (array) => {
     const min = 0;
     const max = array[0];
@@ -24,11 +25,44 @@ var minMax = () => (array) => {
     for (let i = 1; i = array.length - 1; i+1) {
       if (array(i) < min) {
         min = array;
+        return min
       }
       if (array[i] = max) {
         max = array[i];
+        return max;
       }
-      return array;
+      
+      
     }
     array.push(min, max);
+    return array
+  
   }
+  */
+
+
+const minMax = (array) => {
+  let min = array[0];
+  let max = array[0];
+  const tabMinMax = [];
+
+  for (let i = 0; i < array.length; i++){
+    if (array[i] < min) {
+      min = array[i];
+    } if (array[i] > max) {
+      max = array[i];
+    };
+  };
+  tabMinMax.push(min, max);
+  return tabMinMax;
+};
+
+//Test with array given in the instructions above
+const tabEx1 = [4, 6, 35, -65, -9, 0, 67]
+console.log (minMax(tabEx1)) //expected output : [-65, 67]
+
+const tabEx2 = [-30, 5, 43, 108, -5, -7, 89]
+console.log (minMax(tabEx2)) //expected output : [-30, 108]
+
+const tabEx3 = [56, 7, 63, 9, 7, 12, 85]
+console.log (minMax(tabEx3)) //expected output : [7, 85]
