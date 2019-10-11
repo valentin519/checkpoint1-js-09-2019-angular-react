4629342981146cb3098811b5f28186afaed84501
irvintarlao <irvin.tarlao@gmail.com>
2019-10-11 10:29:07 +0200

    algo2.js résolu

--- algo2.js
+++ algo2.js
@@ -17,18 +17,59 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
+// var minMax = () => (array) => {
+//     const min = 0;
+//     const max = array[0];
   
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
-        max = array[i];
-      }
-      return array;
-    }
-    array.push(min, max);
-  }
+//     for (let i = 1; i = array.length - 1; i+1) {
+//       if (array(i) < min) {
+//         min = array;
+//       }
+//       if (array[i] = max) {
+//         max = array[i];
+//       }
+//       return array;
+//     }
+//     array.push(min, max);
+//   }
+
+
+let tab1 = [4, 6, 35, -65, -9, 0, 67];
+let tab2 = [-30, 5, 43, 108, -5, -7, 89];
+let tab3 = [56, 7, 63, 9, 7, 12, 85];
+
+let newArray = []
+
+function minMax(array) {
+    newArray.push(plusPetiteValeur(array), plusGrandeValeur(array))
+    return newArray;
+}
+
+function plusGrandeValeur(array)
+{
+	let max = 0;
+	for (i = 0; i < array.length; i++)
+	{
+		if (array[i] > max)
+		{
+			max=array[i];
+		}
+	}
+	return max;
+}
+
+function plusPetiteValeur(array)
+{
+	let min = array.length;
+	for (i = 0; i < array.length; i++)
+	{
+		if (array[i] < min)
+		{
+			min=array[i];
+		}
+	}
+	return min;
+}
+
+console.log(minMax(tab1))
+
