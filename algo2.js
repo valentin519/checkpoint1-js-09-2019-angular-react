4574a6e00db6b6b78b1dbc09cfef911d12bfe4e7
Tanguy Duponchelle <tanguy.duponchelle@hotmail.fr>
2019-10-11 10:14:51 +0200

    algo 2 done, totally modified

--- algo2.js
+++ algo2.js
@@ -17,22 +17,11 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = array[0]
-    const max = array[0];
-  
-    for (let i = 0; i <array.length; i++) {
-      if (array[i] < min) {
-        min = array[i];
-      }
-      if (array[i] > max) {
-        max = array[i];
-      }
-      console.log(min, max)
-      return min, max
-    }
-    array.push(min, max);
-    
-  }
-
-  minMax()
+const minMax = () => {
+  const results = []
+let min = Math.min(...array)
+let max = Math.max(...array)
+results.push(min, max)
+return results
+}
+minMax()
