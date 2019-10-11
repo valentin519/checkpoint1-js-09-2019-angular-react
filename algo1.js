4671195dc745d121d51cf50abc80fec97a3b9679
Fadi Rata <fadi.rata@live.fr>
2019-10-11 13:06:52 +0200

    Fin exam

--- algo1.js
+++ algo1.js
@@ -17,3 +17,16 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+function theatre(siege,colonnes){
+  let arrayB=[];
+  for (let i = 1; i < siege.length; i++ ){
+    let arrayA=[]
+    for (let j = 1; j < colonnes.length; j++){
+      let elem = ${siege} - ${colonnes};
+      arrayA.push(elem);
+    }
+    arrayB.push(arrayA);
+  }
+  return arrayB;
+}
