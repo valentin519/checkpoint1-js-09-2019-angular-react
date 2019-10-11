ce6b0629da47ddcb79b0c916de5d93b7fb7820d0
Herve Bourelle <herve.bourelle@gmail.fr>
2019-10-11 11:04:47 +0200

    solution algo1

--- algo1.js
+++ algo1.js
@@ -17,3 +17,12 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+function numberPlace() {
+const row = 27
+const col = 101
+	for (let i = 1; i < row; i++) {
+    for (let j =1; j < col; j++) {
+			console.log( i+'-'+j)
+		}
+	}
+}
