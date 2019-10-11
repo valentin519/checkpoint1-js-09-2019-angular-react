c88715cfb3159d335a7e16c10e4ad2045319d931
Aurelien Perraudin <orelien.perraudin@gmail.com>
2019-10-11 15:05:40 +0200

    algo2

--- algo2.js
+++ algo2.js
@@ -17,15 +17,15 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
+var minMax = () => {
+    const min = 100000;
+    const max = 0;
   
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
+    for (let i = 0; i <= array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
       }
-      if (array[i] = max) {
+      if (array[i] > max) {
         max = array[i];
       }
       return array;
