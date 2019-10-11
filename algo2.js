bafec8fb77cea36a3e1bd90e68e3faa76ceb6282
nassilio <55001600+nassilio@users.noreply.github.com>
2019-10-11 13:11:51 +0200

    Add files via upload

--- algo2.js
+++ algo2.js
@@ -17,18 +17,18 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
+const minMax = array => {
+    let min = array[0];
+    let max = array[0];
   
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
       }
-      if (array[i] = max) {
+      if (array[i] > max) {
         max = array[i];
-      }
-      return array;
+      }  
     }
-    array.push(min, max);
-  }
+    let arrayResult = [min, max]
+    return arrayResult;
+}
