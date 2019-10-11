58187a853fcb9afd88b096b2c900d0f64d897373
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-10-11 10:25:21 +0200

    corrected algo1 (chair started at 0) and did algo2.js

--- algo1.js
+++ algo1.js
@@ -18,7 +18,7 @@ Au final la liste devra ressembler à :
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
 
-for (let i = 0 ; i < 26 ; i++ ) {
+for (let i = 1 ; i <= 26 ; i++ ) {
    const siegeTheatre = []
    for (let j = 1 ; j <= 100 ; j ++) {
      siegeTheatre.push( `${i} - ${j} `);
--- algo2.js
+++ algo2.js
@@ -17,18 +17,39 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
+// var minMax = () => (array) => {
+//     const min = 0;
+//     const max = array[0];
   
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
-      }
-      if (array[i] = max) {
-        max = array[i];
-      }
-      return array;
-    }
-    array.push(min, max);
-  }
+//     for (let i = 1; i = array.length - 1; i+1) {
+//       if (array(i) < min) {
+//         min = array;
+//       }
+//       if (array[i] = max) {
+//         max = array[i];
+//       }
+//       return array;
+//     }
+//     array.push(min, max);
+//   }
+
+const minMax = (array) => {
+  newArray = [];
+  let min = 0;
+  let max = array[0];
+
+  for (let i = 0 ; i < array.length ; i++) {
+    
+    if (array[i] <= min) {
+      min = array[i];
+    } if (array[i] >= max ) {
+      max = array[i];
+    };
+  };
+  
+  newArray.push(min,max);
+  console.log(newArray);  
+}
+
+// minMax([4, 6, 35, -65, -9, 0, 67]);
+// minMax([-30, 5, 43, 108, -5, -7, 89]);
