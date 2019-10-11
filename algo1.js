927d782b89653b07ab8bb771afa25f78f919369c
Titouan Colas <tit.colas@gmail.com>
2019-10-11 10:21:45 +0200

    1er commit et solution de l'algo1

--- algo1.js
+++ algo1.js
@@ -1,19 +1,26 @@
-/*
-Un employé de théatre voudrait afficher la liste de tous les sièges présents dans sa salle principale.
-Dans la salle les sièges sont répartis de la manière suivantes : 
-  - Il y a 26 colonnes de sièges, numérotés de "1" à "26"
-  - Chaque colonne contient 100 sièges, numérotés de "1" à "100"
+/* Un employé de théatre voudrait afficher la liste de tous les sièges présents dans sa salle principale. Dans la salle les sièges sont répartis de la manière suivantes :    - Il y a 26 colonnes de sièges, numérotés de "1" à "26"   - Chaque colonne contient 100 sièges, numérotés de "1" à "100"  Au final la liste devra ressembler à :  1-1  1-2 1-3 . . . 26-98 26-99 26-100 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.*/
 
-Au final la liste devra ressembler à : 
-1-1 
-1-2
-1-3
-.
-.
-.
-26-98
-26-99
-26-100
-
-Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
+/* Le fonction seatCount prend 2 parametres : le nbColumn (nombre de colonnes de la salle) et nbRaw (nombre de rangées).
+Le nombre total d'élément du tableau est donc nbColumn * nbRaw.
+Elle utilise seatColumn et seatRaw comme compteur pour les valeurs de chaque case de l'array seatList.
+Dans la boucle for, elle incremente les rangées, et si seatRaw dépasse 100, on passe a la colonne suivante(c'est a dire réinitialiser seatRaw à 1 et incrementer seatColumn).
+seatCount ajoute au debut de chaque tour de boucle la valeur "seatColumn-seatRaw" a seatList.
+elle finit par rendre seatList.
 */
+
+seatCount = (nbColumn, nbRaw) => {
+  let seatColumn = 1;
+  let seatRaw = 1;
+  let seatList = [];
+  for(i=0; i < nbRaw*nbColumn; i++){
+    seatList.push(seatColumn + "-" + seatRaw);
+    seatRaw++;
+    if(seatRaw > 100){
+      seatRaw = 1;
+      seatColumn++;
+    }
+  }
+  return(seatList);
+}
+
+console.log(seatCount(26,100))
