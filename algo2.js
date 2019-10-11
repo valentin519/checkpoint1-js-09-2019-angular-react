fa35d550b7efadf77dd7989120964d2d7a3fc795
Julie Hellec <hellecjulie@gmail.com>
2019-10-11 11:13:38 +0200

    Partie 1 algo2 OK

--- algo2.js
+++ algo2.js
@@ -16,19 +16,24 @@
 * Tableau d'entrée : [56, 7, 63, 9, 7, 12, 85]
 * Résultat : [7, 85]
 */
+let result=[];
+let exemple= [56, 7, 63, 9, 7, 12, 85];
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
+let minMax = (newArr,array) => {
+    let min = array[0];
+    let max = array[0];
   
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
+    for (let i = 0; i <array.length; i++) {
+      if (array[i] < min || array[i] === min) {
+        min = array[i];
       }
-      if (array[i] = max) {
+      if (array[i] > max || array[i] === max ) {
         max = array[i];
       }
-      return array;
-    }
-    array.push(min, max);
+    };
+    newArr.push(min, max);
+    console.log(newArr);
   }
+
+  minMax(result,exemple)
+  
