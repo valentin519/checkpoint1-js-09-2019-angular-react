f0653b5af895b3eee0a7eba8b517e0605cb1d86d
Martin Klauer <mdklauer@gmail.com>
2019-10-11 11:08:53 +0200

    Resolution algo2

--- algo2.js
+++ algo2.js
@@ -17,18 +17,24 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
-  
-    for (let i = 1; i = array.length - 1; i+1) {
-      if (array(i) < min) {
-        min = array;
+
+var minMax = (array) => {
+  let newMinMax = [];
+    let min = array[0];
+    let max = array[0];
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i]
+        console.log(min)
       }
-      if (array[i] = max) {
-        max = array[i];
+      if (array[i] >= max) {
+        max = array[i]
+        console.log(max)
       }
-      return array;
+     
     }
-    array.push(min, max);
+    newMinMax.push(min, max);
+    return newMinMax;
+    
   }
