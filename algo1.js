912fe5636156f7d4987d90922426ac649ee01c99
Marine Incera <marine.incera@gmail.com>
2019-10-12 21:31:57 +0200

    modif algo 1

--- algo1.js
+++ algo1.js
@@ -18,16 +18,20 @@ Au final la liste devra ressembler à :
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
 
-let room = [];
 
-for (let i = 1; i < 27; i++) {
-  let col = [];
-  for (let j = 1; j < 101; j++) {
-    let row = i;
-    col.push(i + "-" + j);
+const displayRoom = () => {
+  let room = [];
+  for (let i = 1; i < 27; i++) {
+    let col = [];
+    for (let j = 1; j < 101; j++) {
+      let row = i;
+      col.push(i + "-" + j);
 
+    }
+    room.push(col);
   }
-  room.push(col);
+
+  return room;
 }
 
-console.log(room);
+displayRoom();
