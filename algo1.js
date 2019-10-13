8572ef17bbe88f3ed34c28a538da2eff50b21431
Tim <Tim@MacBook-Pro-de-Tim.local>
2019-10-13 12:33:02 +0200

    resolve algo1

--- algo1.js
+++ algo1.js
@@ -17,3 +17,14 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+const tab = () => {
+  for(let i = 1; i <= 26 ; i++){
+      for(let j = 1; j <=100 ;j++){
+        console.log(`${i} - ${j}`)
+      }
+    }
+  }
+
+tab();
+
