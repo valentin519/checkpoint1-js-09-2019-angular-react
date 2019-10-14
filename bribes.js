f03ce1d2709477d7710438e49bb0ee9a1ffac6d8
Dindonneau <bourdoncle.p@gmail.com>
2019-10-14 09:20:18 +0200

    JS ok but no TOTAL

--- bribes.js
+++ bribes.js
@@ -1,4 +1,26 @@
+const allBribes=[];
+
+
 function writeItDown() {
-    var writeName = document.getElementById('name');
-    var writeSum = document.getElementById('paid');
+
+    var writeName = document.getElementById('name').value;
+    var writeSum = document.getElementById('paid').value;
+
+    const bribesEntry = {
+        name: writeName, 
+        sum: writeSum
+    };
     
+    allBribes.push(bribesEntry);
+    
+    const sumTot = () => {
+        let total = 0;
+        for (i=0;I<allBribes.length;i++) {
+            total = total + allBribes[i].sum;
+        }
+    };
+        
+
+    return document.getElementById('entries').innerHTML = `${allBribes.map(tagada => `<div>${tagada.name} ${tagada.sum}</div>`)}`;
+    //return document.getElementById('total').innerHTML = `<div>TOTAL :  ${sumTot()}</div>`;
+};
